fb0a82c1e05f74c6bbbb4d928ee1f5fceeca17b0
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2024-08-21 18:03:52 +0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    // https://eslint.vuejs.org/rules/component-name-in-template-casing.html
+    // always use kebab case for html tag.
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      {
+        registeredComponentsOnly: false
+      }
+    ],
+    // https://eslint.vuejs.org/rules/html-self-closing.html
+    // never use /> for html tag and vue component, leave void self closing to prettier.
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          normal: 'always',
+          component: 'always',
+          void: 'never'
+        }
+      }
+    ],
+    'prefer-const': 'error'
+  }
+}
---
+++ src/pages/home/page-home.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>halaman home</div>
+</template>
