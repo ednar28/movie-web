2391fdc09d0136e5c845680ae7b6aceb8a3e8328
Rizky Putra Ednar <49565820+ednar28@users.noreply.github.com>
2024-08-21 23:51:11 +0700

    setup icon, warna primary, container, tambah basic ssl

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
+import { addDynamicIconSelectors } from '@iconify/tailwind'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      aspectRatio: {
+        banner: '5 / 1.8',
+      },
+      colors: {
+        primary: {
+          default: '#e91e63',
+          50: '#fef1f7',
+          100: '#fee5f0',
+          200: '#fecce3',
+          300: '#ffa2cb',
+          400: '#fe68a6',
+          500: '#f83c85',
+          600: '#e91e63',
+          700: '#cb0b47',
+          800: '#a70d3b',
+          900: '#8b1034',
+          950: '#55021a',
+        },
+      },
+    },
   },
-  plugins: []
+  plugins: [addDynamicIconSelectors()],
 }
